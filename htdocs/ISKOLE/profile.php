18170d88f6b998f44f292dd33bfa0694ada2ebe1
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2020-11-04 11:39:40 +0530

    working on with how to redirect login to profile

---
+++ htdocs/ISKOLE/profile.php
@@ -0,0 +1,18 @@
+<?php 
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>profile</title>
+</head>
+<body>
+	<?php
+		header();
+		echo "Hashan";
+		print_r($_SESSION);
+	?>
+
+</body>
+</html>
