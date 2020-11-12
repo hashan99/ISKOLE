9be05e1c89637794f439e952ce6d5abcc2c661be
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2020-11-12 21:26:01 +0530

    admin and cm login added

--- htdocs/ISKOLE/Login/login.php
+++ htdocs/ISKOLE/Login/login.php
@@ -15,6 +15,7 @@
 
     <?php
 
+      // login
       // if(isset($_POST['submitLogin']))
       // {
         if(isset($_POST['email']) AND isset($_POST['password']) == true)
@@ -23,73 +24,128 @@
             $pw = $_POST['password'];
             $enPw = sha1($pw);
 
+            $dbQueryAdmin = "SELECT * FROM admin WHERE username='$email' AND password='$enPw'";
+            $dbQueryCM = "SELECT * FROM content_manager WHERE username='$email' AND password='$enPw'";
             $dbQueryStudent = "SELECT * FROM student WHERE email='$email' AND password='$enPw'";
             $dbQueryTeacher = "SELECT * FROM teacher WHERE email='$email' AND password='$enPw'";
-            // $dbQueryAdmin = "SELECT * FROM admin WHERE username='$email' AND password='$enPw'";
 
+            $resultAdmin = mysqli_query($con, $dbQueryAdmin);
+            $resultCM = mysqli_query($con, $dbQueryCM);
             $resultStudent = mysqli_query($con, $dbQueryStudent);
             $resultTeacher = mysqli_query($con, $dbQueryTeacher);
-            // $resultAdmin = mysqli_query($con, $dbQueryAdmin);
+            
 
-            if($_POST['email']=="admin" AND $_POST['password']=="admin123")
+
+            // if($_POST['email']=="admin" AND $_POST['password']=="admin123")
+            // {
+            //   echo "Welcome Admin!";
+            // }
+            // else if($_POST['email']=="content-manager" AND $_POST['password']=="cm123")
+            // {
+            //   echo "Welcome Content-Manager!";
+            // }
+            if($resultAdmin)
             {
-              echo "Welcome Admin!";
-            }
-            else if($_POST['email']=="content-manager" AND $_POST['password']=="cm123")
-            {
-              echo "Welcome Content-Manager!";
-            }
-            else if($resultStudent)
-            {
-              $count = mysqli_num_rows($resultStudent);
-              // echo mysqli_num_rows($resultStudent);
+              $count = mysqli_num_rows($resultAdmin);
+              // echo mysqli_num_rows($resultTeacher);
               // echo "<br>";
                     
-              while($row = mysqli_fetch_assoc($resultStudent))
+              while($row = mysqli_fetch_assoc($resultAdmin))
               {
                 echo "<pre>";
                 print_r($row);
                 echo "</pre><br>";
-                $loggedUser = $row['first_name'];
+                $loggedUser = $row['username'];
                 // echo $loggedUser;
               }
 
               if($count==1)
               {
                 echo "Logged as ".$loggedUser."!!";
+                // header('Location: http://localhost/HRMS/profile.php');
               }
               else
               {
                 // $error="email or password is invalid";
-                // echo "Login Failed!!";
-                if($resultTeacher)
+                if($resultCM)
                 {
-                  $count = mysqli_num_rows($resultTeacher);
-                  // echo mysqli_num_rows($resultTeacher);
+                  $count = mysqli_num_rows($resultCM);
+                  // echo mysqli_num_rows($resultStudent);
                   // echo "<br>";
                     
-                  while($row = mysqli_fetch_assoc($resultTeacher))
+                  while($row = mysqli_fetch_assoc($resultCM))
                   {
                     echo "<pre>";
                     print_r($row);
                     echo "</pre><br>";
-                    $loggedUser = $row['first_name'];
+                    $loggedUser = $row['username'];
                     // echo $loggedUser;
                   }
 
                   if($count==1)
                   {
                     echo "Logged as ".$loggedUser."!!";
-                    // header('Location: http://localhost/HRMS/profile.php');
                   }
                   else
                   {
                     // $error="email or password is invalid";
-                    echo "Login Failed!!";
+                    // echo "Login Failed!!";
+                    if($resultStudent)
+                    {
+                      $count = mysqli_num_rows($resultStudent);
+                      // echo mysqli_num_rows($resultStudent);
+                      // echo "<br>";
+                    
+                      while($row = mysqli_fetch_assoc($resultStudent))
+                      {
+                        echo "<pre>";
+                        print_r($row);
+                        echo "</pre><br>";
+                        $loggedUser = $row['first_name'];
+                        // echo $loggedUser;
+                      }
+
+                      if($count==1)
+                      {
+                        echo "Logged as ".$loggedUser."!!";
+                      }
+                      else
+                      {
+                        // $error="email or password is invalid";
+                        // echo "Login Failed!!";
+                        if($resultTeacher)
+                        {
+                          $count = mysqli_num_rows($resultTeacher);
+                          // echo mysqli_num_rows($resultTeacher);
+                          // echo "<br>";
+                    
+                          while($row = mysqli_fetch_assoc($resultTeacher))
+                          {
+                            echo "<pre>";
+                            print_r($row);
+                            echo "</pre><br>";
+                            $loggedUser = $row['first_name'];
+                            // echo $loggedUser;
+                          }
+
+                          if($count==1)
+                          {
+                            echo "Logged as ".$loggedUser."!!";
+                            // header('Location: http://localhost/HRMS/profile.php');
+                          }
+                          else
+                          {
+                            // $error="email or password is invalid";
+                            echo "Login Failed!!";
+                          }
+                        }  
+                      }
+                    }  
                   }
-                }  
+                }
               }
-            }
+            }  
+            
         }      
       // }
       // else
