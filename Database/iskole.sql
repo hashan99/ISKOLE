46668d58c9415d2b45c9be8cd8450866f9ffabca
NirushiWijesiri <56571293+NirushiWijesiri@users.noreply.github.com>
2020-11-14 14:22:27 +0530

    Database added

--- Database/iskole.sql
+++ Database/iskole.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2020 at 02:14 PM
--- Server version: 10.3.16-MariaDB
--- PHP Version: 7.2.19
+-- Generation Time: Nov 14, 2020 at 09:46 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -46,6 +45,30 @@ INSERT INTO `admin` (`ad_id`, `username`, `email`, `status`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `availability`
+--
+
+CREATE TABLE `availability` (
+  `availability_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `content`
+--
+
+CREATE TABLE `content` (
+  `content_id` int(11) NOT NULL,
+  `content` varchar(10000) NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `cm_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `content_manager`
 --
 
@@ -63,6 +86,97 @@ CREATE TABLE `content_manager` (
 
 INSERT INTO `content_manager` (`cm_id`, `username`, `email`, `status`, `password`) VALUES
 (1, 'content-manager', 'content-manager@gmail.com', 1, '71d60c2ae2e120e7e3b2060c29abde8a14bb2c2f');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forum`
+--
+
+CREATE TABLE `forum` (
+  `forum_id` int(11) NOT NULL,
+  `forum_name` varchar(100) NOT NULL,
+  `subject_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `forum_reply_thread`
+--
+
+CREATE TABLE `forum_reply_thread` (
+  `thread_id` int(11) NOT NULL,
+  `forum_id` int(11) NOT NULL,
+  `reply_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `get_help`
+--
+
+CREATE TABLE `get_help` (
+  `availability_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grade`
+--
+
+CREATE TABLE `grade` (
+  `grade_id` int(11) NOT NULL,
+  `cm_id` int(11) NOT NULL,
+  `ad_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `profile_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `ad_id` int(11) NOT NULL,
+  `cm_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quiz`
+--
+
+CREATE TABLE `quiz` (
+  `quiz_id` int(11) NOT NULL,
+  `mark` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `cm_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reply`
+--
+
+CREATE TABLE `reply` (
+  `reply_id` int(11) NOT NULL,
+  `reply_date` date NOT NULL,
+  `reply_message` varchar(10000) NOT NULL,
+  `up_votes` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `children` int(11) NOT NULL,
+  `thread_id` int(11) NOT NULL,
+  `forum_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -97,6 +211,18 @@ INSERT INTO `student` (`student_id`, `first_name`, `last_name`, `email`, `passwo
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `subject`
+--
+
+CREATE TABLE `subject` (
+  `subject_id` int(11) NOT NULL,
+  `subject_name` varchar(20) NOT NULL,
+  `ad_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `teacher`
 --
 
@@ -119,6 +245,36 @@ INSERT INTO `teacher` (`teacher_id`, `first_name`, `last_name`, `email`, `passwo
 (4, 'Keerthi', 'Dharmasiri', 'keerthi@gmail.com', '86023c3e1097461fd460a64aba6115b2a3ec1035', 1),
 (6, 'chandana', 'jaasinghe', 'chandana@gmail.com', 'ecff3deb1db198fa450173a79bc961332b667dc6', 0);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thread`
+--
+
+CREATE TABLE `thread` (
+  `thread_id` int(11) NOT NULL,
+  `t_date` date NOT NULL,
+  `t_description` varchar(10000) NOT NULL,
+  `t_title` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `ad_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `topic`
+--
+
+CREATE TABLE `topic` (
+  `topic_id` int(11) NOT NULL,
+  `topic_name` varchar(100) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `ad_id` int(11) NOT NULL,
+  `cm_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
@@ -130,10 +286,78 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`ad_id`);
 
 --
+-- Indexes for table `availability`
+--
+ALTER TABLE `availability`
+  ADD PRIMARY KEY (`availability_id`),
+  ADD KEY `teacher_id` (`teacher_id`);
+
+--
+-- Indexes for table `content`
+--
+ALTER TABLE `content`
+  ADD PRIMARY KEY (`content_id`),
+  ADD KEY `cm_id` (`cm_id`),
+  ADD KEY `topic_id` (`topic_id`);
+
+--
 -- Indexes for table `content_manager`
 --
 ALTER TABLE `content_manager`
   ADD PRIMARY KEY (`cm_id`);
+
+--
+-- Indexes for table `forum`
+--
+ALTER TABLE `forum`
+  ADD PRIMARY KEY (`forum_id`),
+  ADD KEY `subject_id` (`subject_id`);
+
+--
+-- Indexes for table `forum_reply_thread`
+--
+ALTER TABLE `forum_reply_thread`
+  ADD PRIMARY KEY (`thread_id`,`forum_id`,`reply_id`);
+
+--
+-- Indexes for table `get_help`
+--
+ALTER TABLE `get_help`
+  ADD PRIMARY KEY (`availability_id`,`student_id`);
+
+--
+-- Indexes for table `grade`
+--
+ALTER TABLE `grade`
+  ADD PRIMARY KEY (`grade_id`),
+  ADD KEY `ad_id` (`ad_id`),
+  ADD KEY `cm_id` (`cm_id`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`profile_id`),
+  ADD KEY `ad_id` (`ad_id`),
+  ADD KEY `cm_id` (`cm_id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `teacher_id` (`teacher_id`);
+
+--
+-- Indexes for table `quiz`
+--
+ALTER TABLE `quiz`
+  ADD PRIMARY KEY (`quiz_id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `cm_id` (`cm_id`);
+
+--
+-- Indexes for table `reply`
+--
+ALTER TABLE `reply`
+  ADD PRIMARY KEY (`reply_id`),
+  ADD KEY `forum_id` (`forum_id`),
+  ADD KEY `thread_id` (`thread_id`);
 
 --
 -- Indexes for table `student`
@@ -142,10 +366,35 @@ ALTER TABLE `student`
   ADD PRIMARY KEY (`student_id`);
 
 --
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`subject_id`),
+  ADD KEY `ad_id` (`ad_id`);
+
+--
 -- Indexes for table `teacher`
 --
 ALTER TABLE `teacher`
   ADD PRIMARY KEY (`teacher_id`);
+
+--
+-- Indexes for table `thread`
+--
+ALTER TABLE `thread`
+  ADD PRIMARY KEY (`thread_id`),
+  ADD KEY `ad_id` (`ad_id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `teacher_id` (`teacher_id`);
+
+--
+-- Indexes for table `topic`
+--
+ALTER TABLE `topic`
+  ADD PRIMARY KEY (`topic_id`),
+  ADD KEY `ad_id` (`ad_id`),
+  ADD KEY `cm_id` (`cm_id`),
+  ADD KEY `subject_id` (`subject_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -158,10 +407,46 @@ ALTER TABLE `admin`
   MODIFY `ad_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `availability`
+--
+ALTER TABLE `availability`
+  MODIFY `availability_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `content`
+--
+ALTER TABLE `content`
+  MODIFY `content_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `content_manager`
 --
 ALTER TABLE `content_manager`
   MODIFY `cm_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `forum`
+--
+ALTER TABLE `forum`
+  MODIFY `forum_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `quiz`
+--
+ALTER TABLE `quiz`
+  MODIFY `quiz_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `reply`
+--
+ALTER TABLE `reply`
+  MODIFY `reply_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `student`
@@ -170,10 +455,103 @@ ALTER TABLE `student`
   MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
+-- AUTO_INCREMENT for table `subject`
+--
+ALTER TABLE `subject`
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `teacher`
 --
 ALTER TABLE `teacher`
   MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `thread`
+--
+ALTER TABLE `thread`
+  MODIFY `thread_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `topic`
+--
+ALTER TABLE `topic`
+  MODIFY `topic_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `availability`
+--
+ALTER TABLE `availability`
+  ADD CONSTRAINT `availability_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`);
+
+--
+-- Constraints for table `content`
+--
+ALTER TABLE `content`
+  ADD CONSTRAINT `content_ibfk_1` FOREIGN KEY (`cm_id`) REFERENCES `content_manager` (`cm_id`),
+  ADD CONSTRAINT `content_ibfk_2` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`topic_id`);
+
+--
+-- Constraints for table `forum`
+--
+ALTER TABLE `forum`
+  ADD CONSTRAINT `forum_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`);
+
+--
+-- Constraints for table `grade`
+--
+ALTER TABLE `grade`
+  ADD CONSTRAINT `grade_ibfk_1` FOREIGN KEY (`ad_id`) REFERENCES `admin` (`ad_id`),
+  ADD CONSTRAINT `grade_ibfk_2` FOREIGN KEY (`cm_id`) REFERENCES `content_manager` (`cm_id`);
+
+--
+-- Constraints for table `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `profile_ibfk_1` FOREIGN KEY (`ad_id`) REFERENCES `admin` (`ad_id`),
+  ADD CONSTRAINT `profile_ibfk_2` FOREIGN KEY (`cm_id`) REFERENCES `content_manager` (`cm_id`),
+  ADD CONSTRAINT `profile_ibfk_3` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`),
+  ADD CONSTRAINT `profile_ibfk_4` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`);
+
+--
+-- Constraints for table `quiz`
+--
+ALTER TABLE `quiz`
+  ADD CONSTRAINT `quiz_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`),
+  ADD CONSTRAINT `quiz_ibfk_2` FOREIGN KEY (`cm_id`) REFERENCES `content_manager` (`cm_id`);
+
+--
+-- Constraints for table `reply`
+--
+ALTER TABLE `reply`
+  ADD CONSTRAINT `reply_ibfk_1` FOREIGN KEY (`forum_id`) REFERENCES `forum` (`forum_id`),
+  ADD CONSTRAINT `reply_ibfk_2` FOREIGN KEY (`thread_id`) REFERENCES `thread` (`thread_id`);
+
+--
+-- Constraints for table `subject`
+--
+ALTER TABLE `subject`
+  ADD CONSTRAINT `subject_ibfk_1` FOREIGN KEY (`ad_id`) REFERENCES `admin` (`ad_id`);
+
+--
+-- Constraints for table `thread`
+--
+ALTER TABLE `thread`
+  ADD CONSTRAINT `thread_ibfk_1` FOREIGN KEY (`ad_id`) REFERENCES `admin` (`ad_id`),
+  ADD CONSTRAINT `thread_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `subject` (`subject_id`),
+  ADD CONSTRAINT `thread_ibfk_3` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`);
+
+--
+-- Constraints for table `topic`
+--
+ALTER TABLE `topic`
+  ADD CONSTRAINT `topic_ibfk_1` FOREIGN KEY (`ad_id`) REFERENCES `admin` (`ad_id`),
+  ADD CONSTRAINT `topic_ibfk_2` FOREIGN KEY (`cm_id`) REFERENCES `content_manager` (`cm_id`),
+  ADD CONSTRAINT `topic_ibfk_3` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
