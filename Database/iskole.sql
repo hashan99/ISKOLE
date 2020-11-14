8af5467f93cb0b2bbbca2cc405545312ec8366cd
NirushiWijesiri <56571293+NirushiWijesiri@users.noreply.github.com>
2020-11-14 16:54:59 +0530

    Database Updated

--- Database/iskole.sql
+++ Database/iskole.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 14, 2020 at 09:46 AM
+-- Generation Time: Nov 14, 2020 at 12:22 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
