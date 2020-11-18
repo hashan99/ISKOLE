5b289a977c537058a911cffffd9ee7bb2b0f6970
Amaya <amayakinivita97@gmail.com>
2020-11-18 20:29:42 +0530

    Topic 1

--- htdocs/ISKOLE/Content Manager/ContentManager Home Page/Submit Content/SubmitContent.php
+++ htdocs/ISKOLE/Content Manager/ContentManager Home Page/Submit Content/SubmitContent.php
@@ -3,18 +3,18 @@
 	$Subject = $_POST['Subject'];
 	$Topic_no = $_POST['Topic_no'];
 	$Topic = $_POST['Topic'];
-	$Furthur_Reading = $_POST['Furthur_Reading'];
-	$File = $_POST['File'];
-	
+	$Further_Reading = $_POST['Further_Reading'];
+	$Presentation = $_POST['Presentation'];
+	$Areas = $_POST['Areas'];
 
 	// Database connection
-	$connection = new mysqli('localhost','root','','project');
+	$connection = new mysqli('localhost','root','','iskole');
 	if($connection->connect_error){
 		echo "$connection->connect_error";
 		die("Connection Failed : ". $connection->connect_error);
 	} else {
-		$stmt = $connection->prepare("insert into content_new(Grade, Subject, Topic_no, Topic, Furthur_Reading, File) values(?, ?, ?, ?, ?, ?)");
-		$stmt->bind_param("isisss", $Grade, $Subject, $Topic_no, $Topic, $Furthur_Reading, $File);
+		$stmt = $connection->prepare("insert into content(Grade, Subject, Topic_no, Topic, Further_Reading, Presentation, Areas) values(?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("isissss", $Grade, $Subject, $Topic_no, $Topic, $Further_Reading, $Presentation, $Areas);
 		$execval = $stmt->execute(); 
 		echo $execval;
 		echo "Content added successfully!";
