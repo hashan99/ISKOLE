335c8ef302225179f061fb455c2a36f31178775f
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2020-11-24 00:17:18 +0530

    thread.sql added

--- Database/thread.sql
+++ Database/thread.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2020 at 01:10 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.11
+-- Generation Time: Nov 23, 2020 at 07:45 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.2.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -29,23 +30,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `thread` (
   `thread_id` int(11) NOT NULL,
-  `t_date` date DEFAULT NULL,
-  `t_description` varchar(10000) DEFAULT NULL,
-  `t_title` varchar(100) DEFAULT NULL,
+  `t_date` date NOT NULL,
+  `t_description` varchar(5000) NOT NULL,
+  `t_title` varchar(200) NOT NULL,
   `student_id` int(11) DEFAULT NULL,
   `teacher_id` int(11) DEFAULT NULL,
   `ad_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `thread`
 --
 
 INSERT INTO `thread` (`thread_id`, `t_date`, `t_description`, `t_title`, `student_id`, `teacher_id`, `ad_id`) VALUES
-(7, '2020-11-19', 'I ve read in a recent blog that toothpaste is acidic? Is this true?', 'Why toothpaste is acidic?', NULL, NULL, NULL),
-(8, '2020-11-19', ',jvljhb;kjopjpip', 'yguyguguhuihuhi', NULL, NULL, NULL),
-(9, '2020-11-19', ',jvljhb;kjopjpip', 'yguyguguhuihuhi', NULL, NULL, NULL),
-(10, '2020-11-22', 'Lets see how it goes', 'This is a new question', NULL, NULL, NULL);
+(2, '2020-11-23', 'Let\'s see how this goes.', 'This is my first question.', NULL, NULL, NULL),
+(3, '2020-11-23', 'I think sky is actually not blue.', 'Why is the sky blue?', NULL, NULL, NULL),
+(4, '2020-11-23', 'I think i\'m correct always.', 'Am i correct?', NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -55,10 +55,7 @@ INSERT INTO `thread` (`thread_id`, `t_date`, `t_description`, `t_title`, `studen
 -- Indexes for table `thread`
 --
 ALTER TABLE `thread`
-  ADD PRIMARY KEY (`thread_id`),
-  ADD KEY `ad_id` (`ad_id`),
-  ADD KEY `student_id` (`student_id`),
-  ADD KEY `teacher_id` (`teacher_id`);
+  ADD PRIMARY KEY (`thread_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -68,7 +65,7 @@ ALTER TABLE `thread`
 -- AUTO_INCREMENT for table `thread`
 --
 ALTER TABLE `thread`
-  MODIFY `thread_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `thread_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
