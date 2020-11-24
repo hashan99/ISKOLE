f621edbe8cf4a4ea036f8e84a6b1ddf7533be6f3
Amaya <amayakinivita97@gmail.com>
2020-11-24 11:27:16 +0530

    content.sql

--- Database/content.sql
+++ Database/content.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2020 at 03:08 PM
+-- Generation Time: Nov 24, 2020 at 06:54 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
